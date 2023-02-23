9769d74d15b13e452bc8a8d81ad297bbec75fdf4
Software <96375724+Asilbekrasul@users.noreply.github.com>
2023-02-23 15:01:33 +0300

    new

---
+++ app.js
@@ -0,0 +1,67 @@
+const refresh = document.querySelector('.refresh')
+const clear = document.querySelector('#clear')
+const complate = document.querySelector('#complate')
+const li = document.querySelector('li')
+
+let myNodelist = document.getElementsByTagName("LI");
+let = i;
+for (i = 0; i < myNodelist.length; i++) {
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+let close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        let div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+// Add a "checked" symbol when clicking on a list item
+let list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+},);
+
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("myInput").value;
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("Iltimos biron narsa kiriting")
+    } else {
+        document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            let div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
+
+clear.addEventListener('click' , ()=>{
+    document.querySelector("#myUL").classList.add('hidden')
+})
+complate.addEventListener("click" , ()=>{
+    li.classList.toggle('checked')
+})
